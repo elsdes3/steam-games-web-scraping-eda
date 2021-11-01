2b6fab85a3d097b6c79fe556947750877db09596
elsdes3 <92226676+elsdes3@users.noreply.github.com>
2021-11-01 12:52:55 -0400

    Update papermill_runner.py

--- papermill_runner.py
+++ papermill_runner.py
@@ -99,14 +99,14 @@ def run_notebooks(
 if __name__ == "__main__":
     nb_dict_list = [
         nine_dict,
-        # six_dict,
-        # seven_dict,
-        ten_dict
+        six_dict,
+        seven_dict,
+        ten_dict,
     ]
     nb_name_list = [
         nine_dict_nb_name,
-        # six_dict_nb_name,
-        # seven_dict_nb_name,
+        six_dict_nb_name,
+        seven_dict_nb_name,
         ten_dict_nb_name,
     ]
     notebook_list = [
